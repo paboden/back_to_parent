a69a35d8e034383ad247b78a095982c3b8ca193c
Gabe <gabe@chapterthreellc.com>
2014-08-01 17:43:52 -0700

    Debug, add template funcitonality

--- templates/back_to_parent_block.tpl.php
+++ templates/back_to_parent_block.tpl.php
@@ -3,9 +3,12 @@
 /**
  * @file
  * Template for Back to Parent link.
+ * Variables:
+ *   $back_to_parent_link
+ *     contains the parent menu item array, and block config settings
  */
 ?>
 
 <?php if (isset($back_to_parent_link)) : ?>
-  <?php print render($back_to_parent_link);?>
+  <?php print l($back_to_parent_link['title'], $back_to_parent_link['href']);?>
 <?php endif; ?>
